5bffb7e72fa545122f3a47f02db456536610dc52
Enrico Sacchetti <esacchetti@gmail.com>
2017-03-29 22:08:33 -0700

    feat: add proper default test

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,20 +1,39 @@
 // Simple test
 
 var gulp = require('gulp')
+var gutil = require('gulp-util')
 var resourceHints = require('../lib')
 var sequence = require('run-sequence')
 var tap = require('tap')
-// var fs = require('fs')
+var compare = require('compare-with-file').fileMatches
+var Transform = require('stream').Transform
 
-gulp.task('images', function (cb) {
+gulp.task('defaultTest', function (cb) {
   return gulp.src('./fixtures/*(.html)!(*-result.html)')
     .pipe(resourceHints())
     .pipe(gulp.dest('./results'))
+    .pipe(new Transform({
+      objectMode: true,
+      transform: function (file, enc, cb) {
+        var resultFile = `./fixtures/${file.relative.replace('.html', '-result.html')}`
+        if (compare(resultFile, String(file.contents))) {
+          tap.passing()
+        } else {
+          tap.fail('Default Test: File does not match expected result')
+        }
+
+        cb(null, file)
+      }
+    }))
     .on('end', () => {
-      tap.pass('This is fine')
+      tap.pass('Default Test: Pass')
+    })
+    .on('error', () => {
+      gutil.log()
+      tap.fail('Default Test: Error on stream')
     })
 })
 
 gulp.task('default', function (cb) {
-  sequence('images')
+  sequence('defaultTest')
 })
